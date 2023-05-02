5d27cfd38460d7d5d89190e86e41c0aeee5590b9
Jonathan Breidfjord <jbreidfjord@gmail.com>
2023-05-02 10:52:28 -0600

    clippy

--- src/loxide/resolver.rs
+++ src/loxide/resolver.rs
@@ -71,14 +71,13 @@ impl Resolver {
         statements.iter().try_for_each(|stmt| self.visit_stmt(stmt))
     }
 
-    fn resolve_local(&mut self, expr: &Expr, name: &Token) -> Result {
+    fn resolve_local(&mut self, expr: &Expr, name: &Token) {
         for (i, scope) in self.scopes.iter().enumerate().rev() {
             if scope.contains_key(&name.get_lexeme()) {
                 let distance = self.scopes.len() - 1 - i;
                 self.locals.insert(expr.clone(), distance);
             }
         }
-        Ok(())
     }
 
     fn declare(&mut self, name: &Token) -> Result {
@@ -92,11 +91,10 @@ impl Resolver {
         Ok(())
     }
 
-    fn define(&mut self, name: &Token) -> Result {
+    fn define(&mut self, name: &Token) {
         if let Some(scope) = self.scopes.last_mut() {
             scope.insert(name.get_lexeme(), true);
         }
-        Ok(())
     }
 
     fn resolve_function(&mut self, declaration: &FunctionDeclaration, fn_type: FnType) -> Result {
@@ -106,7 +104,7 @@ impl Resolver {
         self.begin_scope();
         for param in &declaration.params {
             self.declare(param)?;
-            self.define(param)?;
+            self.define(param);
         }
         self.resolve(&declaration.body)?;
         self.end_scope();
@@ -125,15 +123,17 @@ impl Visitor<Result, Result> for Resolver {
                         return Err(Error::SelfReferencedInitializer);
                     }
                 }
-                self.resolve_local(expr, name)
+                self.resolve_local(expr, name);
+                Ok(())
             }
 
             Expr::Assign { name, value } => {
                 self.visit_expr(value)?;
-                self.resolve_local(expr, name)
+                self.resolve_local(expr, name);
+                Ok(())
             }
 
-            Expr::Binary { left, right, .. } => {
+            Expr::Binary { left, right, .. } | Expr::Logical { left, right, .. } => {
                 self.visit_expr(left)?;
                 self.visit_expr(right)
             }
@@ -148,11 +148,6 @@ impl Visitor<Result, Result> for Resolver {
             Expr::Grouping(expr) => self.visit_expr(expr),
 
             Expr::Literal(_) => Ok(()),
-
-            Expr::Logical { left, right, .. } => {
-                self.visit_expr(left)?;
-                self.visit_expr(right)
-            }
 
             Expr::Unary { right, .. } => self.visit_expr(right),
 
@@ -174,16 +169,17 @@ impl Visitor<Result, Result> for Resolver {
                 if let Some(initializer) = initializer {
                     self.visit_expr(initializer)?;
                 }
-                self.define(name)
+                self.define(name);
+                Ok(())
             }
 
             Stmt::Function(declaration) => {
                 self.declare(&declaration.name)?;
-                self.define(&declaration.name)?;
+                self.define(&declaration.name);
                 self.resolve_function(declaration, FnType::Function)
             }
 
-            Stmt::Expression(expr) => self.visit_expr(expr),
+            Stmt::Expression(expr) | Stmt::Print(expr) => self.visit_expr(expr),
 
             Stmt::If {
                 condition,
@@ -197,8 +193,6 @@ impl Visitor<Result, Result> for Resolver {
                 }
                 Ok(())
             }
-
-            Stmt::Print(expr) => self.visit_expr(expr),
 
             Stmt::Return { value, .. } => {
                 if self.current_fn == FnType::None {
